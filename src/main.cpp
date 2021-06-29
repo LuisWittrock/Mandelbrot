4cdd42665b9498bf773d0bd22f12abcd41be68da
luis wittrock <luwitt111@gmail.com>
2021-06-29 16:02:40 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+class Frame
+{
+    public:
+};
+
+class Mandelbrot
+{
+    public:
+
+};
+
+int main()
+{
+
+}
